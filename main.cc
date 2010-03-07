a4764ffa2ddf714bb5ccdc4cb7d526e09f86cbcc
Tapio Vierros <tapiovierros@gmail.com>
2010-03-07 17:08:41 +0200

    Diagonal movement with numpad.

--- main.cc
+++ main.cc
@@ -15,10 +15,14 @@ bool handleInput(Actor& pl) {
 	int k = getch();
 
 	if      (k == KEY_ESCAPE || k == 'q') return false;
-	else if (k == KEY_LEFT   || k == 'a') pl.move(-1,0);
-	else if (k == KEY_RIGHT  || k == 'd') pl.move(1,0);
-	else if (k == KEY_UP     || k == 'w') pl.move(0,-1);
-	else if (k == KEY_DOWN   || k == 's') pl.move(0,1);
+	else if (k == KEY_LEFT   || k == '4') pl.move(-1,0);
+	else if (k == KEY_RIGHT  || k == '6') pl.move(1,0);
+	else if (k == KEY_UP     || k == '8') pl.move(0,-1);
+	else if (k == KEY_DOWN   || k == '2') pl.move(0,1);
+	else if (k == '7') pl.move(-1,-1);
+	else if (k == '9') pl.move( 1,-1);
+	else if (k == '1') pl.move(-1, 1);
+	else if (k == '3') pl.move( 1, 1);
 	else if (k == '?' || k == 'h') help();
 	return true;
 }
