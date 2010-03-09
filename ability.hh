f0770cee980570d967ca76dd9fa91f68efa0001c
Tapio Vierros <tapiovierros@gmail.com>
2010-03-09 16:59:51 +0200

    Some Ability stuff.

--- ability.hh
+++ ability.hh
@@ -10,19 +10,27 @@ class Ability {
   public:
 	enum TargetType { SELF, ACTOR, WORLD } targetType;
 
+	Ability(): hidden(false) { }
+
 	virtual bool operator()(Actor* target, bool force = false) { return false; }
+	virtual bool operator()(Actor* self, Tile* target, bool force = false) { return false; }
 	virtual bool operator()(Tile* target, bool force = false) { return false; }
 
 	virtual std::string toString() const { return "Generic Ability"; }
+
+	bool hidden;
 };
 
 typedef boost::ptr_vector<Ability> Abilities;
 
+
+
 class Ability_OpenDoor: public Ability {
-  public:
 	bool operator()(Tile* target, bool force = false);
 	std::string toString() const { return "Open door"; }
 };
 class Ability_CloseDoor: public Ability { };
 class Ability_KickDoor: public Ability { };
 class Ability_Talk: public Ability { };
+
+class Ability_DemonFire: public Ability { };
