d208c5ed25cbf6d78a11ac8bd43fa4febc7d407a
Tapio Vierros <tapiovierros@gmail.com>
2010-03-10 03:03:47 +0200

    Peek through windows only from distance 1.

--- common.cc
+++ common.cc
@@ -4,7 +4,7 @@ Tile TileBuilder(std::string type) {
 	Tile tile;
 	if (type == "window") {
 		tile = Tile('-', COLOR_BLUE, BLOCKS);
-		tile.blocks_vision_dist = 2;
+		tile.blocks_vision_dist = 1;
 	} else if (type == "door_open") {
 		tile = Tile('/', COLOR_YELLOW, !BLOCKS);
 	} else if (type.substr(0,4) == "door") {
