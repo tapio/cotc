3e43237b4a221cef9c0d57ab2a1321d0b7993c97
Tapio Vierros <tapiovierros@gmail.com>
2010-10-17 21:49:32 +0300

    Improve generate-version.sh

--- generate-version.sh
+++ generate-version.sh
@@ -5,14 +5,20 @@
 # Note: This is run automatically by CMake, don't do it manually.
 # ***
 
+MAJOR=1  # Major release git tag should point to commit updating this
 VERSION_H="version.h"
 
-V=`git log --pretty=oneline | wc -l`""
-V=`echo "$V-139"|bc`
-echo "#define VERSION_STRING \"1.$V\"" > $VERSION_H.new
+BASECOMMIT=`git show $MAJOR.0 | head -1 | cut -d " " -f 2`
+BASECOMMITS=`git log $BASECOMMIT --pretty=oneline | wc -l`""
+TOTALCOMMITS=`git log --pretty=oneline | wc -l`""
+
+MINOR=$(($TOTALCOMMITS - $BASECOMMITS))
+
+echo "#pragma once" > $VERSION_H.new
+echo "#define VERSION_STRING \"$MAJOR.$MINOR\"" >> $VERSION_H.new
 
 if [ -f $VERSION_H ] ; then
-	# Check if they differ
+	# Check if they differ (to avoid changing time stamps if not necessary)
 	cmp -s $VERSION_H $VERSION_H.new
 	if [ $? -eq 1 ] ; then
 		mv $VERSION_H.new $VERSION_H
